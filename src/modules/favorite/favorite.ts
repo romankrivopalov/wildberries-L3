c09df5544b6714c9f59381be00ffb3e8bc33dffb
Work desktop <the.red.lemon@yandex.ru>
2023-10-20 15:55:08 +0300

    feat: adds favorite product count and control via localstorage

---
+++ src/modules/favorite/favorite.ts
@@ -0,0 +1,11 @@
+import { Component } from '../component';
+import html from './favorite.tpl.html';
+
+class Favotite extends Component {
+
+  async render() {
+    
+  }
+}
+
+export const favoriteComp = new Favotite(html);
---
+++ src/services/favorite.service.ts
@@ -0,0 +1,59 @@
+import localforage from 'localforage';
+import { ProductData } from 'types';
+
+const FV = '__wb-favorite';
+
+class FavoriteService {
+  init() {
+    this._updCounters();
+  }
+
+  async addProduct(product: ProductData) {
+    const products = await this.get();
+    await this.set([...products, product]);
+  }
+
+  async removeProduct(product: ProductData) {
+    const products = await this.get();
+    await this.set(products.filter(({ id }) => id !== product.id));
+  }
+
+  // async clear() {
+  //   await localforage.removeItem(DB);
+  //   this._updCounters();
+  // }
+
+  async get(): Promise<ProductData[]> {
+    return (await localforage.getItem(FV)) || [];
+  }
+
+  async set(data: ProductData[]) {
+    await localforage.setItem(FV, data);
+    this._updCounters();
+  }
+
+  async isInFavorite(product: ProductData) {
+    const products = await this.get();
+    return products.some(({ id }) => id === product.id);
+  }
+
+  private async _updCounters() {
+    const products = await this.get();
+    const count = products.length >= 10 ? '9+' : products.length;
+    const button = document.querySelector('.favorite, .header__buttons')
+
+    if (!count) {
+      //@ts-ignore
+      button.style.display = 'none'
+    } else {
+      //@ts-ignore
+      button.style.display = 'block'
+    }
+
+    //@ts-ignore
+    document.querySelectorAll('.js__favorite-counter').forEach(($el: HTMLElement) => ($el.innerText = String(count || '')));
+  }
+}
+
+export const favoriteService = new FavoriteService();
+  
