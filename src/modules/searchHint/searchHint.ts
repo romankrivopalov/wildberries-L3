96ddf992acb284d977c78e20f1351795cbc8720c
Home <home@gmail.com>
2023-10-23 22:20:41 +0300

    feat: adds searchHint component

--- src/modules/searchHint/searchHint.ts
+++ src/modules/searchHint/searchHint.ts
@@ -1,14 +1,37 @@
+import { ViewTemplate } from '../../utils/viewTemplate';
+import { View } from '../../utils/view';
 import html from './searchHint.tpl.html';
-import { Component } from '../component';
+import { ProductData } from 'types';
 
-class searchHint extends Component {
+export class searchHint {
+  view: View;
+  products: ProductData[];
 
-  init() {
-
+  constructor(html: any) {
+    this.products = [];
+    this.view = new ViewTemplate(html).cloneView();
   }
 
-  render() {
+  attach($root: HTMLElement) {
+    $root.innerHTML = '';
+    $root.appendChild(this.view.root);
+    this.init();
+  }
 
+  async init() {
+    this.view.example.innerHTML = `например,&nbsp;`;
+    this.view.firstHint.textContent = 'чехол iphone 13 pro';
+    this.view.secondHint.textContent = 'коляски agex';
+    this.view.thirdHint.textContent = 'яндекс станция 2';
+  }
+
+  render(products?: ProductData[]) {
+    if (products) {
+      this.view.example.textContent = '';
+      this.view.firstHint.textContent = products[0];
+      this.view.secondHint.textContent = products[1];
+      this.view.thirdHint.textContent = products[2];
+    }
   }
 }
 
