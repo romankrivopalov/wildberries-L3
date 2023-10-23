27e39ba82d9212d05da8c9caaa760edcbc3ae21a
Work desktop <the.red.lemon@yandex.ru>
2023-10-23 16:56:11 +0300

    feat: adds initial styles for searchHint comp

---
+++ src/modules/searchHint/searchHint.ts
@@ -0,0 +1,15 @@
+import html from './searchHint.tpl.html';
+import { Component } from '../component';
+
+class searchHint extends Component {
+
+  init() {
+
+  }
+
+  render() {
+
+  }
+}
+
+export const searchHintComp = new searchHint(html);
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import { notFoundComp } from './modules/notFound/notFound';
 import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
+import { searchHintComp } from './modules/searchHint/searchHint'; 
 
 const ROUTES = {
   '/': homepageComp,
   '/catalog': catalogComp,
   '/product': productDetailComp,
-  '/checkout': checkoutComp
+  '/checkout': checkoutComp,
+  '/search-hint': searchHintComp
 };
 
 export default class Router {
